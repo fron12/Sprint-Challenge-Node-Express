cdae455ee5eae870ae99f5c20bae152d378e7128
Fred <sohnf90@yahoo.com>
2018-09-07 09:22:29 -0700

    More Setup: cors, helmet, actionsRoute, and projectsRoute

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const server = express();
 
+const actionsRoutes = require('./data/actionsRoutes');
+const projectsRoutes = require('./data/projectsRoutes');
+
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.use('/actions', actionsRoutes);
+server.use('/projects', projectsRoutes);
 
 server.get('/', (req, res) => {
     res.send('Hello');
